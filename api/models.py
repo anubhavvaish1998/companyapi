abc8db94bccd219efd77bfcf8fab7aff5458a594
Anubhav <anubhavvaish04@gmail.com>
2025-03-26 17:37:44 +0530

    second commit

--- api/models.py
+++ api/models.py
@@ -8,3 +8,12 @@ class Company(models.Model):
     type = models.CharField(max_length=100, choices=(('IT','IT'),('Non IT','Non IT')))
     added_date=models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+class Employe(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    about = models.TextField()
+    position = models.CharField(max_length=100,choices=(('Manager','manager'),('Software Developer','SD'),('Team Leader','TL')))
+
--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework import serializers # type: ignore
+from rest_framework import serializers
 from api.models import Company
 
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
     class Meta:
         model = Company
         fields="__all__"
